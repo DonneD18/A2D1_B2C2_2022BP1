a3ce5ec1e46c518edae4b7777997cd5edbb1de97
DonneD18 <166110248+DonneD18@users.noreply.github.com>
2024-10-01 13:47:38 +0200

    .

---
+++ ToDoList/ApplicationProgrammingInterfaceToDoList/Data/JsonDal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ToDoListModel.Models;
+
+namespace ToDoList
+{
+    public class JsonDal : DbContext
+    {
+        public JsonDal (DbContextOptions<JsonDal> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ToDoListModel.Models.ToDoTask> ToDoTask { get; set; } = default!;
+    }
+}
